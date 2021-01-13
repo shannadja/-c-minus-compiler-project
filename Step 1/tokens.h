e414e89ad7c4b302eeb77dc64494ea408dc251d8
Stefanos Hannadjas <77225466+shannadja@users.noreply.github.com>
2021-01-14 01:46:52 +0200

    Add files via upload

---
+++ Step 1/tokens.h
@@ -0,0 +1,42 @@
+
+#ifndef TOKENS_H_INCLUDED
+#define TOKENS_H_INCLUDED
+
+
+#define T_ID 1
+#define T_NUM 2
+#define T_Letter 3
+#define T_Dig 4
+#define T_white 5
+#define T_Comment 6
+#define IF 7
+#define ELSE 8
+#define INT 9
+#define VOID 10
+#define RETURN 11
+#define WHILE 12
+#define PLUS 13
+#define MINUS 14
+#define DIVIDE 15
+#define TIMES 16
+#define GREATEQ 17
+#define SMALL 18
+#define SMALLEQ 19
+#define GREAT 20
+#define EQUALITY 21
+#define NOTEQ 22
+#define COMMA 23
+#define LEFTPAR 24
+#define RIGHTPAR 25
+#define LBRAC 26
+#define RBRAC 27
+#define EQUAL 28
+#define QUESTMARK 29
+#define LCURLY 30
+#define RCURLY 31
+#define LCOMM 32
+#define RCOMM 33
+#define CHAR 34
+
+#endif
+
